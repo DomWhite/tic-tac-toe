182336a1f3d8409393bf255248d1d569eba941c3
Dominic White <dominicpaulwhite@gmail.com>
2015-03-25 22:47:13 +1100

    refined css and added images

--- javascript/app.js
+++ javascript/app.js
@@ -8,43 +8,95 @@ var game = {
 	player: "X",
 	row:'',
 	col:'',
+	counterX: 0,
+	counterO: 0,
+	moveCount: 0,
+
+
 	playerMove: function() {
-		grid[game.row][game.col] = game.player;
-		game.checkWin()
-		if (game.player === "X") {
-			game.player = "O";
+		if (game.moveCount < 10) {
+			grid[game.row][game.col] = game.player;
+			game.checkWin()
+			if (game.player === "X") {
+				game.player = "O";
+			} else if (game.player === "O") {
+				game.player = "X";
+			}
 		} else {
-			game.player = "X";
+			reset.resetAll();
+			reset.resetBoard();
 		}
 	},
 
+	checkWin: function() {
+		if (grid[0][0] === grid[0][1] && grid[0][1] === grid[0][2]) {			//Row 1
+			game.addAndClear();
+		} else if (grid[1][0] === grid[1][1] && grid[1][1] === grid[1][2]) {	//Row 2
+			game.addAndClear();
+		} else if (grid[2][0] === grid[2][1] && grid[2][1] === grid[2][2]) {	//Row 3
+			game.addAndClear();
+		} else if (grid[0][0] === grid[1][0] && grid[1][0] === grid[2][0]) {	//Col 1
+			game.addAndClear();
+		} else if (grid[0][1] === grid[1][1] && grid[1][1] === grid[2][1]) {	//Col 2
+			game.addAndClear();
+		} else if (grid[0][2] === grid[1][2] && grid[1][2] === grid[2][2]) {	//Col 3
+			game.addAndClear();
+		} else if (grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]) {	//Diag TL to BR
+			game.addAndClear();
+		} else if (grid[2][0] === grid[1][1] && grid[1][1] === grid[0][2]) {	//Diag TR to BL
+			game.addAndClear();
+		}
+	},
+
+	addScore: function() {
+			if (game.player = "X") {
+				game.counterX += 1;
+				document.getElementById("scoreX").innerHTML = game.counterX;
+			} else if (game.player = "O") {
+				game.counterO += 1;
+				document.getElementById("scoreO").innerHTML = game.counterO;
+			}
+	},
+
+	addAndClear: function() {
+			game.addScore();
+			setTimeout(function() {
+				reset.resetBoard();
+				reset.resetAll();
+			}, 1000);
+	},
+}
+
+var reset = {
+	resetAll: function() {
+		game.row = '';
+		game.col = '';
+		game.counterX = 0;
+		game.counterY = 0;
+		game.moveCount = 0;
+
+
+	},
 	resetBoard: function() {
 		grid = [
 				["tl", "tm", "tr"],
     			["ml", "mm", "mr"],
     			["bl", "bc", "br"],
-    			]
+    			];
+    	$(".gameCell").html("_");
 	},
 
-	checkWin: function() {
-		if (grid[0][0] === grid[0][1] && grid[0][1] === grid[0][2]) {			//Row 1
-			console.log("player " + game.player + " wins! row 1");
-		} else if (grid[1][0] === grid[1][1] && grid[1][1] === grid[1][2]) {	//Row 2
-			console.log("player " + game.player + " wins! row 2");
-		} else if (grid[2][0] === grid[2][1] && grid[2][1] === grid[2][2]) {	//Row 3
-			console.log("player " + game.player + " wins! row 3");
-		} else if (grid[0][0] === grid[1][0] && grid[1][0] === grid[2][0]) {	//Col 1
-			console.log("player " + game.player + " wins! col 1");
-		} else if (grid[0][1] === grid[1][1] && grid[1][1] === grid[2][1]) {	//Col 2
-			console.log("player " + game.player + " wins! col 2");
-		} else if (grid[0][2] === grid[1][2] && grid[1][2] === grid[2][2]) {	//Col 3
-			console.log("player " + game.player + " wins! col 3");
-		} else if (grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]) {	//Diag TL to BR
-			console.log("player " + game.player + " wins! diag1");
-		} else if (grid[2][0] === grid[1][1] && grid[1][1] === grid[0][2]) {	//Diag TR to BL
-			console.log("player " + game.player + " wins! diag 2");
-		}
-	}
+	resetScores: function() {
+		$("#scoreX").html("_")
+		$("#scoreO").html("_")
+	},
+
+
+}
+
+
+var redRound = {
+
 }
 
 window.onload = function() {
@@ -52,14 +104,18 @@ window.onload = function() {
 	$(".gameCell").click(function() {
 		var $location = $(this).attr("id");
 		var arrLoc = $location.split("-");
-		// var $location = $(this).attr("id").split("-"); //gets id of clicked item and splits 
-		// game.row = parseInt($location[0]); 
-		// game.col = parseInt($location[1]);
 		game.row = parseInt(arrLoc[0]); 
 		game.col = parseInt(arrLoc[1]);
 		console.log(game.row, game.col);
 		document.getElementById($location).innerHTML = game.player;
+		game.moveCount += 1
 		game.playerMove();
+	})
+
+	$("#resetBtn").click(function(){
+		reset.resetAll();
+		reset.resetBoard();
+		reset.resetScores();
 	})
 
 }
@@ -69,22 +125,3 @@ window.onload = function() {
 
 
 
-// var player = 1;
-
-
-// function clickBtn(btn) {
-// 	var btnElem = document.getElementById(btn);
-// 	if (player === 1) {
-// 		btnElem.value = "X";
-// 		btnElem.disabled = "disabled";
-// 		player -= 1;
-// 	} else {
-// 		btnElem.value = "O";
-// 		btnElem.disabled = "disabled";
-// 		player += 1;
-// 	}
-// }
-
-// function winner(){
-// 	if (document.getElementById(btn).value = "X") {}
-// }
